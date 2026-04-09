179bf2246b246e32d36e474b7c7338f6416d1aa5
RAHI BOT BBY <rahiahmed143@gmail.com>
2026-04-09 20:45:12 +0600

    Update credit in info.js to reflect new developer

--- plugins/info.js
+++ plugins/info.js
@@ -7,7 +7,7 @@ module.exports = {
     permission: 0,
     prefix: 'both',
     categorie: 'Utilities',
-    credit: 'Developed by Mohammad Nayan',
+    credit: 'Developed by Mohammad Rahi',
     usages: [`${global.config.PREFIX}info - Show admin and server information.`],
   },
   start: async ({ event, api, message }) => {
