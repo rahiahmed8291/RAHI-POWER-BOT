cf9298ed443aa09f42a89e110687eca7f4835ac6
RAHI BOT BBY <rahiahmed143@gmail.com>
2026-04-21 14:33:27 +0600

    Enhance goodbye message for removed members

--- grpUpCmd/gdbye.js
+++ grpUpCmd/gdbye.js
@@ -2,11 +2,23 @@ module.exports = {
   event: 'remove',
   handle: async ({ api, event }) => {
     const removedMembers = event.participants;
+    
     for (const member of removedMembers) {
+      const username = `@${member.split('@')[0]}`;
+      
+      const goodbyeMessage = 
+        `╭━━━〔 🚪 𝗟𝗘𝗙𝗧 / 𝗥𝗘𝗠𝗢𝗩𝗘𝗗 〕━━━╮\n` +
+        `┃\n` +
+        `┃ 👤 𝗨𝘀𝗲𝗿: ${username}\n` +
+        `┃ 📉 𝗦𝘁𝗮𝘁𝘂𝘀: Disconnected\n` +
+        `┃ 💔 𝗠𝗲𝘀𝘀𝗮𝗴𝗲: Goodbye! We hope you \n` +
+        `┃    had a great time here.\n` +
+        `┃\n` +
+        `╰━━━━━━━━━━━━━━━━━━━━━━━╯\n` +
+        `   ✨ 𝗣𝗼𝘄𝗲𝗿𝗲𝗱 𝗕𝘆: 𝗥𝗮𝗵𝗶 𝗣𝗮𝗽𝗮 ✨`;
+
       await api.sendMessage(event.id, {
-        text: `Goodbye @${member.split('@')[0]}, we'll miss you
-        
-       > 𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝚁𝙰𝙷𝙸!`,
+        text: goodbyeMessage,
         mentions: [member]
       });
     }
